55ef9d8379da99e7d57f6f983cb3148fc8cac2e8
Luke Benstead <kazade@gmail.com>
2013-10-07 09:08:21 +0100

    Tweak the Android.mk file

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 include $(CLEAR_VARS)
-LOCAL_PATH 		:= $(ROOT_PATH)
+LOCAL_PATH 		:= $(ROOT_PATH)/lib/kaztimer
 LOCAL_MODULE	:= kaztimer
-LOCAL_SRC_FILES := $(wildcard $(LOCAL_PATH)/kaztimer/*.c)
+LOCAL_SRC_FILES := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/kaztimer/*.c))
 include $(BUILD_SHARED_LIBRARY) 
