0dd8dc4de33c962d656dab118bdd0d49bdd440e4
Luke Benstead <kazade@gmail.com>
2014-09-27 20:49:47 +0100

    Fix up the Android.mk file

--- Android.mk
+++ Android.mk
@@ -1,5 +1,8 @@
+LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
-LOCAL_PATH 		:= $(ROOT_PATH)/lib/kaztimer
 LOCAL_MODULE	:= kaztimer
-LOCAL_SRC_FILES := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/kaztimer/*.c))
-include $(BUILD_SHARED_LIBRARY) 
+LOCAL_CFLAGS    += "-std=c++11"
+LOCAL_SRC_FILES := $(wildcard $(LOCAL_PATH)/kaztimer/*.cpp)
+LOCAL_SRC_FILES := $(LOCAL_SRC_FILES:$(LOCAL_PATH)/%=%)
+include $(BUILD_SHARED_LIBRARY)
